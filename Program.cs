b68298179386ad16a6f0528080890d1ff4d92a0d
Anoreloth <93758322+Anoreloth@users.noreply.github.com>
2021-12-21 19:58:54 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MKR_1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Random rnd = new Random();
+
+            Console.Write("Введiть n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Чи є серед a(2n+1),..., a(3n) є рівні a1,..,a(2n)?");
+            int[] a = new a[3 * n];
+            for (int i = 1; i <= 3 * n; i++)
+            {
+                a[i] = rnd.Next(1, 100);
+
+
+            }
+            for (int j = 0; j < 3 * n; j++)
+            {
+                if (a[2 * i + 1] == a[i])
+                {
+                    Console.Write("Члени рівні ");
+                }
+            }
+
+        }
+    }
+}
